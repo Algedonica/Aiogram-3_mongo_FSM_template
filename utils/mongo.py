c5f6b664b28147ee420b703906b5e68b5aadc59c
Nick Liar <83134187+Algedonica@users.noreply.github.com>
2023-02-27 02:40:37 +0300

    Replace pymongo to motor for mongo

--- utils/mongo.py
+++ utils/mongo.py
@@ -1,4 +1,4 @@
-from pymongo import MongoClient
+from motor.motor_asyncio import AsyncIOMotorClient
 from aiogram.fsm.storage.base import BaseStorage, StorageKey, StateType
 from aiogram.fsm.state import State
 from aiogram import Bot
@@ -8,7 +8,7 @@ from typing import Any, Dict, Optional, Union
 
 class MongoStorage(BaseStorage):
     def __init__(self, uri: str, database: str, collection_states: str) -> None:
-        self.client = MongoClient(uri)
+        self.client = AsyncIOMotorClient(uri)
         self.db = self.client[database]
         self.collection = self.db[collection_states]
 
@@ -16,20 +16,21 @@ class MongoStorage(BaseStorage):
         data = await self.get_data(bot, key)
         data['state'] = state
         final_state=resolve_state(data['state'])
-        self.collection.update_one({'key': key.__dict__}, {'$set': {"state":final_state}}, upsert=True)
+        await self.collection.update_one({'key': key.__dict__}, {'$set': {"state":final_state}}, upsert=True)
 
 
     async def get_state(self, bot: Bot, key: StorageKey) -> Optional[str]:
-        data = self.collection.find_one({'key': key.__dict__})
+        data = await self.collection.find_one({'key': key.__dict__})
+        print(data)
         if data:
             return data.get('state')
         return None
 
     async def set_data(self, bot: Bot, key: StorageKey, data: Dict[str, Any]) -> None:
-        self.collection.update_one({'key': key.__dict__}, {'$set': {"data":data}}, upsert=True)
+        await self.collection.update_one({'key': key.__dict__}, {'$set': {"data":data}}, upsert=True)
 
     async def get_data(self, bot: Bot, key: StorageKey) -> Dict[str, Any]:
-        data = self.collection.find_one({'key': key.__dict__})
+        data = await self.collection.find_one({'key': key.__dict__})
         if data:
             return data.get('data')
         else:
@@ -40,7 +41,7 @@ class MongoStorage(BaseStorage):
     ) -> Dict[str, Any]:
         if data:
             kwargs.update(data)
-        self.collection.update_one({'key': key.__dict__}, {'$set': {"data":kwargs}}, upsert=True)
+        await self.collection.update_one({'key': key.__dict__}, {'$set': {"data":kwargs}}, upsert=True)
         return kwargs
 
     async def close(self) -> None:  # pragma: no cover
@@ -48,10 +49,6 @@ class MongoStorage(BaseStorage):
         Close storage (database connection, file or etc.)
         """
         pass
-
-    # async def clear(self) -> None:
-    #     await self.set_state(state=None)
-    #     await self.set_data({})
 
 
 
@@ -64,5 +61,3 @@ def resolve_state(value):
         return value
 
 
-
-# state.key = StorageKey(bot_id=botid, chat_id=chat_id, user_id=userid, destiny='default')
